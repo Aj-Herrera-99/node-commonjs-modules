2ba68823069e1280bf6f529007a3b9dc84f2559e
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-29 16:29:53 +0100

    hobbies

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+function buildHobbies(hobbyOne, hobbyTwo, hobbyThree){
+    return ({
+        hobbies: [hobbyOne, hobbyTwo, hobbyThree]
+    })
+}
+
+console.table(buildHobbies("mangiare", "dormire", "giocare"));
